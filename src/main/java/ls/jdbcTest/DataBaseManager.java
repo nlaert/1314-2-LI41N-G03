fed14be8d193ed797012845158620f254355684b
Nick Laert <nlaert@gmail.com>
2014-03-13 16:41:21 +0000

    jdbc test concluded

---
+++ src/main/java/ls/jdbcTest/DataBaseManager.java
@@ -0,0 +1,40 @@
+package jdbcTest;
+import java.sql.*;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+
+public class DataBaseManager {
+
+//	private static final String url = "jdbc:sqlserver://10.211.55.8:1433;user=ls;password=ls1314;databaseName=LS";
+	private static Connection connection;
+	private static SQLServerDataSource ds = new SQLServerDataSource();
+	
+	public DataBaseManager() 
+	{
+//		ds.setServerName("10.211.55.8");
+		ds.setServerName("localhost");
+		ds.setPortNumber(1433);
+		ds.setDatabaseName("LS");
+		ds.setUser("ls");
+		ds.setPassword("ls1314");
+		
+		try{
+			connection = ds.getConnection();
+//			connection = DriverManager.getConnection(url);
+		}
+		catch(SQLException sqlex)
+		{
+			System.out.println(sqlex.getMessage());
+		}
+
+	}
+
+	public void closeConnection() throws SQLException {
+		connection.close();
+	}
+
+	public Connection getConnetion() {
+		return connection;
+	}
+
+}
---
+++ src/main/java/ls/jdbcTest/JDBCTest.java
@@ -0,0 +1,10 @@
+package jdbcTest;
+
+import java.sql.SQLException;
+
+public class JDBCTest {
+	public static void main(String[] args) throws SQLException {
+		new CRUD();
+		
+	}
+}

bda097cbc8f55b8b89367f735b4d8595b55f5321
jprisel <a35392@alunos.isel.pt>
2014-03-13 16:03:59 +0000

    JDBC again

---
+++ src/main/java/App.java
@@ -0,0 +1,13 @@
+import java.sql.SQLException;
+
+
+import ls.jdbc.query.ListarAlunos;
+
+
+
+public class App {
+
+	public static void main(String[] args) throws SQLException{
+		new ListarAlunos();
+	}
+}
---
+++ src/main/java/LS.sql
@@ -0,0 +1,13 @@
+use LS
+
+drop table Aluno
+
+create table Aluno(
+	bi int primary key,
+	nome varchar(100)
+	)
+	
+insert into Aluno values
+			(1,'Joao'),
+			(2,'Nick'),
+			(3,'Catia');
